bf56fc639dfe862df51a47945a4fe2a13ddb8d25
Ryhnbriant <90240547+Ryhnbriant@users.noreply.github.com>
2022-03-04 19:10:19 +0700

    Add files via upload

---
+++ TugasPBO1/src/Tugas/PBO1.java
@@ -0,0 +1,12 @@
+package Tugas;
+
+public class PBO1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hallo, Nama Saya Reyhan Brilliant");
+		System.out.println("Saya adalah mahasiswa Teknik Informatika Universitas Dian Nuswantoro Semarang");
+		System.out.println("Saya sedang mencoba menulis Program Java");
+	}
+
+}
