8a5badf2c154914e2101103326b7c12b37ea36f9
Ryhnbriant <90240547+Ryhnbriant@users.noreply.github.com>
2022-03-28 16:55:07 +0700

    Add files via upload

---
+++ week4/MotherboardAksi.java
@@ -0,0 +1,18 @@
+package week4;
+
+import java.util.Scanner;
+
+public class MotherboardAksi {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		Motherboard.USB usb = new Motherboard.USB();
+		usb.usb2 = input.nextInt();
+		usb.usb3 = input.nextInt();
+		System.out.println("Total Ports = " +usb.getTotalPorts());
+		
+
+	}
+}
